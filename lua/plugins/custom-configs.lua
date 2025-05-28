c2a017a2337f45bf62b85ac206c4ae251545dd55
omar-s-ta <omar.sebres@gmail.com>
2025-05-28 16:13:24 +0200

    use LazyVim recipe

--- lua/plugins/custom-configs.lua
+++ lua/plugins/custom-configs.lua
@@ -13,6 +13,17 @@ return {
           },
         },
       },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
     },
   },
 
