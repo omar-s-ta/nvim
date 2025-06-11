9de869a0bad4fa1281233f81b2938270051a6b21
omar-s-ta <omar.sebres@gmail.com>
2025-06-11 16:35:45 +0200

    correctly include ocaml

--- lua/plugins/ocaml.lua
+++ lua/plugins/ocaml.lua
@@ -1,47 +1,47 @@
 return {
   recommended = function()
     return LazyVim.extras.wants({
-      ft = { "ml", "mli", "cmi", "cmo", "cmx", "cma", "cmxa", "cmxs", "cmt", "cmti", "opam" },
-      root = { "merlin.opam", "dune-project" },
+      ft = { "ocaml", "ml", "mli", "cmi", "cmo", "cmx", "cma", "cmxa", "cmxs", "cmt", "cmti", "opam", "dune" },
+      root = { "dune-project", "*.opam", "esy.json", ".merlin" },
     })
   end,
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "ocaml" })
+        vim.list_extend(opts.ensure_installed, { "ocaml", "ocaml_interface" })
       end
     end,
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
-      setup = {
-        ocamllsp = function()
-          local opts = {
-            cmd = { "ocamllsp" },
-            filetypes = {
-              "ocaml",
-              "ocaml.menhir",
-              "ocaml.interface",
-              "ocaml.ocamllex",
-              "reason",
-              "dune",
-            },
-            root_dir = function(fname)
-              return require("lspconfig.util").root_pattern(
-                "*.opam",
-                "esy.json",
-                "package.json",
-                ".git",
-                "dune-project",
-                "dune-workspace",
-                "*.ml"
-              )(fname)
-            end,
-          }
-          require("lspconfig").ocamllsp.setup(opts)
-        end,
+      servers = {
+        ocamllsp = {
+          mason = false,
+          cmd = { "ocamllsp" },
+          filetypes = {
+            "ocaml",
+            "ocaml.menhir",
+            "ocaml.interface",
+            "ocaml.ocamllex",
+            "reason",
+            "dune",
+            "ml",
+            "mli",
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "*.opam",
+              "dune-project",
+              "dune-workspace",
+              "esy.json",
+              "package.json",
+              ".merlin",
+              ".git"
+            )(fname)
+          end,
+        },
       },
     },
   },
