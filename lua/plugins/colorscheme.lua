e9638c7ac0b3eaacf738fa695f4c405685b06831
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-03-02 10:53:15 +0100

    nord theme customization

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,32 @@
 return {
   { "omar-s-ta/colorbuddy.nvim" },
   { "ellisonleao/gruvbox.nvim" },
-  { "arcticicestudio/nord-vim" },
+  {
+    "arcticicestudio/nord-vim",
+    config = function()
+      local set_nord_highlights = function()
+        vim.api.nvim_set_hl(0, "@keyword.function", { fg = "#B48EAD" })
+        vim.api.nvim_set_hl(0, "Keyword", { fg = "#B48EAD" })
+        vim.api.nvim_set_hl(0, "LspInlayHint", {
+          -- fg = "#8FBCBB",
+          fg = "#4C566A",
+          bg = "NONE",
+          italic = false,
+          bold = false,
+          blend = 0,
+        })
+      end
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "nord",
+        callback = set_nord_highlights,
+      })
+
+      if vim.g.colors_name == "nord" then
+        set_nord_highlights()
+      end
+    end,
+  },
   { "sainnhe/gruvbox-material" },
   { "EdenEast/nightfox.nvim" },
   { "vague2k/vague.nvim" },
