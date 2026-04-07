00604818dce57e5db3ee55e7207165c6ae908d47
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-04-07 16:01:02 +0200

    remove illuminate

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,7 +36,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
-    { import = "lazyvim.plugins.extras.editor.illuminate" },
     -- { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.editor.navic" },
