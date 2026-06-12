6cf7a9c8f4645b14cda3b6d40372ec34484beb48
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-06-12 09:43:14 +0200

    include zig

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
 
     { import = "lazyvim.plugins.extras.test.core" },
 
